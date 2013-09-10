5f034ed308b15e6b5ef5c7a5d766189caa4b6093
guile2912 <guile2912@hotmail.com>
2013-09-10 17:15:08 -0400

    rewrite EEPROM calls

--- binding.h
+++ binding.h
@@ -105,7 +105,8 @@ struct bind_data {
 struct rfm22_modem_regs {
   uint32_t bps;
   uint8_t  r_1c, r_1d, r_1e, r_20, r_21, r_22, r_23, r_24, r_25, r_2a, r_6e, r_6f, r_70, r_71, r_72;
-} modem_params[] = {
+} 
+modem_params[] = {
   { 4800, 0x1a, 0x40, 0x0a, 0xa1, 0x20, 0x4e, 0xa5, 0x00, 0x1b, 0x1e, 0x27, 0x52, 0x2c, 0x23, 0x30 }, // 50000 0x00
   { 9600, 0x05, 0x40, 0x0a, 0xa1, 0x20, 0x4e, 0xa5, 0x00, 0x20, 0x24, 0x4e, 0xa5, 0x2c, 0x23, 0x30 }, // 25000 0x00
   { 19200,0x06, 0x40, 0x0a, 0xd0, 0x00, 0x9d, 0x49, 0x00, 0x7b, 0x28, 0x9d, 0x49, 0x2c, 0x23, 0x30 }  // 25000 0x01
@@ -122,28 +123,39 @@ void fatalBlink(uint8_t blinks);
 // Save EEPROM by writing just changed data
 void myEEPROMwrite(int16_t addr, uint8_t data)
 {
+
+  while (!eeprom_is_ready());
+  cli();
+  //if(eeprom_read_word((uint16_t*)addr) != sensorValue) {
+    eeprom_write_block((const void*)&data, (void*)addr, sizeof(data));
+  //}
+  sei();
+  /*
   uint8_t retries = 5;
-  while ((--retries) && (data != EEPROM.read(addr))) {
-    EEPROM.write(addr,data);
+  while ((--retries) && (data !=  eeprom_read_block((const void*)&data, (void*)addr, sizeof(data)))) {
+    //EEPROM.write(addr,data);
+    eeprom_write_block((const void*)&data, (void*)addr, sizeof(data));
   }
   if (!retries) {
     fatalBlink(2);
-  }
+  }*/
 }
 
 int16_t bindReadEeprom()
 {
   uint32_t temp = 0;
-  for (uint8_t i = 0; i < 4; i++) {
+  temp = eeprom_read_dword((uint32_t*) EEPROM_OFFSET);
+  /*for (uint8_t i = 0; i < 4; i++) {
     temp = (temp<<8) + EEPROM.read(EEPROM_OFFSET + i);
-  }
+  }*/
   if (temp!=BIND_MAGIC) {
     return 0;
   }
 
-  for (uint8_t i = 0; i < sizeof(bind_data); i++) {
+  /*for (uint8_t i = 0; i < sizeof(bind_data); i++) {
     *((uint8_t*)&bind_data + i) = EEPROM.read(EEPROM_OFFSET + 4 + i);
-  }
+  }*/
+  eeprom_read_block(&bind_data, EEPROM_OFFSET + 4,sizeof(bind_data));
 
   if (bind_data.version != BINDING_VERSION) {
     return 0;
@@ -276,22 +288,26 @@ void rxWriteEeprom()
 void rxReadEeprom()
 {
   uint32_t temp = 0;
-
+/*
   for (uint8_t i = 0; i < 4; i++) {
     temp = (temp<<8) + EEPROM.read(EEPROM_RX_OFFSET + i);
-  }
+  }*/
+  temp = eeprom_read_dword((uint32_t*)EEPROM_RX_OFFSET);
 
   if (temp!=BIND_MAGIC) {
     Serial.println("RXconf reinit");
     rxInitDefaults();
     rxWriteEeprom();
   } else {
-    for (uint8_t i = 0; i < sizeof(rx_config); i++) {
-      *((uint8_t*)&rx_config + i) = EEPROM.read(EEPROM_RX_OFFSET + 4 + i);
+    /*for (uint8_t i = 0; i < sizeof(rx_config); i++) {
+      *((uint8_t*)&rx_config + i) = EEPROM.read(EEPROM_RX_OFFSET + 4 + i);*/
+      eeprom_read_block(&rx_config, EEPROM_RX_OFFSET + 4,sizeof(rx_config));
     }
     Serial.println("RXconf loaded");
   }
 }
 
 #endif
+
+
 
